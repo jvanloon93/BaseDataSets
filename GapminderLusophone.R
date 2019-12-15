0fcfa7fb945a6944f9629f78c2bbd95852d0bcee
Jvanloon93 <jvanloon93@gmail.com>
2019-12-15 11:39:13 -0500

    final

--- GapminderLusophone.R
+++ GapminderLusophone.R
@@ -2,11 +2,6 @@ library(gapminder)
 library(tidyverse)
 library(ggplot2)
 library(broom)
-
-
-
-#Portuguese Speaking counties vs World Average? 
-
 gpu <- gapminder_unfiltered
 
 Lusophone <- c("Brazil", "Portugal", "Angola", "Mozambique", "Equatorial Guinea", "Guinea-Bissau", "Sao Tome and Principe", "Timor-Leste")
@@ -24,7 +19,7 @@ Francophone <- c("Belgium", "Benin", "Burkina Faso", "Cameroon", "Canada", "Cent
 gpu <- gpu %>%
   mutate(Lang_Group = ifelse(country %in% Lusophone,"Lusophone",
                              ifelse(country %in% Hispanophone, "Hispanophone",
-                                    ifelse(country %in% Francophone, "Francophone", "Non-Romance")))) 
+                                    ifelse(country %in% Francophone, "Francophone", "")))) 
 
 Lusophonedf <- gpu %>%
   filter(Lang_Group == "Lusophone")
@@ -33,35 +28,27 @@ Lusophonedf %>%
   filter(year == 2007) %>%
   arrange(desc(pop))
 
-Lusophonedf %>%
-  filter(year == 2007) %>%
-  arrange(gdpPercap)
-
 ggplot(Lusophonedf, aes(x = year, y = gdpPercap)) + geom_col() +
   facet_wrap(~country)
 
 ggplot(Lusophonedf, aes(x = year, y = lifeExp)) + geom_col() +
   facet_wrap(~country)
 
-ggplot(gpu, aes(x = year, y = gdpPercap)) + geom_point() +
-  facet_wrap(~Lang_Group)
+gdpmodel <- lm(gdpPercap ~ factor(Lang_Group), gpu)
 
-gpurl <- filter(gpu, Lang_Group != "Non-Romance")
+summary(gdpmodel)
 
-gdpmodel <- lm(gdpPercap ~ Lang_Group, gpu)
+lemodel <- lm(lifeExp ~ factor(Lang_Group), gpu)
 
-lemodel <- lm(lifeExp ~ Lang_Group, gpu)
+summary(lemodel)
 
-gdpmodel2 <- lm(gdpPercap ~ Lang_Group, gpurl)
-
-lemodel2 <- lm(lifeExp ~ Lang_Group, gpurl)
+gpurl <- filter(gpu, Lang_Group != "")
 
 ggplot(gpurl, aes(x = year, y = lifeExp, color = Lang_Group)) + geom_point() +
   geom_smooth(method = "lm", se = FALSE)
 
 ggplot(gpurl, aes(x = year, y = gdpPercap, color = Lang_Group)) + geom_point() +
   geom_smooth(method = "lm", se = FALSE)
- 
 
  gpurl %>%
   nest(-Lang_Group) %>%
